ac68590348ccb16e11a82925d604ce4a49b932e1
thairyo <sonthai1310.works@gmail.com>
2022-07-04 21:08:01 +0700

    push project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
   );
 }
 
---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      Support me here!
+      <a href="https://www.youtube.com/watch?v=Ls1IDqJcWd0">https://github.com/thairyo/shopping-cart</a>
+    </div>
+  )
+}
+
+export default About
